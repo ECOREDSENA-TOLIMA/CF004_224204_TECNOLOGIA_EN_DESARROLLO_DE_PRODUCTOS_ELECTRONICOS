a21b5e2bbaa16c1eca4f285a057179d1377fedc6
Francisco Vasquez <fjvasquez8@misena.edu.co>
2023-05-30 17:43:28 -0500

    Falta actividad didáctica
    Falta actividad didáctica

--- src/config/global.js
+++ src/config/global.js
@@ -1,19 +1,10 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
-    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
-    imagenesDecorativasBanner: [
-      {
-        clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
-      },
-      {
-        clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
-      },
-    ],
+    componenteFormativo: 'Identificación de componentes electrónicos',
+    descripcionCurso:
+      'El presente componente formativo contempla temas acerca de los circuitos electrónicos, de cómo están conformados y su vital importancia en la fabricación y operación de los productos electrónicos.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.svg'),
   },
   menuPrincipal: {
     menu: [
@@ -30,28 +21,62 @@ export default {
       },
       {
         nombreRuta: 'tema1',
+        icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Componentes electrónicos',
         desarrolloContenidos: true,
         subMenu: [
           {
+            icono: 'far fa-file-alt',
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Clasificación',
             hash: 't_1_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.2',
+            titulo: 'Simbología',
+            hash: 't_1_2',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.3',
+            titulo: 'Encapsulados',
+            hash: 't_1_3',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.4',
+            titulo: 'Materiales',
+            hash: 't_1_4',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.5',
+            titulo: 'Placa base',
+            hash: 't_1_5',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.6',
+            titulo: 'Características y hojas de datos',
+            hash: 't_1_6',
           },
         ],
       },
 
       {
         nombreRuta: 'tema2',
+        icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Diseño eléctrico y electrónico',
         desarrolloContenidos: true,
       },
       {
         nombreRuta: 'tema3',
+        icono: 'far fa-file-alt',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Órdenes de trabajo',
         desarrolloContenidos: true,
       },
     ],
@@ -97,32 +122,84 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Componentes electrónicos y su simbología',
+      texto:
+        'Arboledas Brihuega, D. (2011). Electrónica básica: guía básica. Ediciones de la U.',
+      tipo: 'Libro',
+      link: 'https://www-ebooks7-24-com.bdigital.sena.edu.co/?il=7120',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Planos esquemáticos',
+      texto:
+        'Ruiz Vázquez, T., y  Arbelaitz Gallego, O. (2004). Análisis Básico de circuitos eléctricos y electrónicos. Pearson Educación.',
+      tipo: 'Libro',
+      link: 'https://www-ebooks7-24-com.bdigital.sena.edu.co/?il=4552',
+    },
+    {
+      tema: 'Materiales',
+      texto:
+        'Carmona Rubio, G. y  Díaz Corcobado, T. (2010). Electrónica aplicada. McGraw-Hill.',
+      tipo: 'Libro',
+      link: 'https://www-ebooks7-24-com.bdigital.sena.edu.co/?il=4852',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Aislantes',
+      significado: 'Materiales que impiden el paso de cargas eléctricas.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Conductores',
+      significado:
+        'Materiales que permiten el movimiento de cargas eléctricas.',
+    },
+    {
+      termino: 'Corriente',
+      significado:
+        'Flujo de electrones a través de un material impulsado por la diferencia de potencial entre dos puntos o materiales.',
+    },
+    {
+      termino: 'Energía eléctrica',
+      significado:
+        'Es una fuente de energía renovable que se obtiene mediante el movimiento de cargas eléctricas (electrones) que se produce en el interior de materiales conductores (por ejemplo, cables metálicos como el cobre).',
+    },
+    {
+      termino: 'Nodo',
+      significado:
+        'Punto de conexión común para dos o más componentes electrónicos que hacen parte de un circuito eléctrico.',
+    },
+    {
+      termino: 'Semiconductores',
+      significado:
+        'Materiales que pueden permitir e impedir el paso de la energía eléctrica.',
+    },
+    {
+      termino: 'Voltaje',
+      significado:
+        'Diferencia de potencial eléctrico entre dos puntos de un circuito o dos materiales con carga eléctrica diferente.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Arboledas Brihuega, D. (2011). Electrónica básica: guía básica. Ediciones de la U.',
+      link:
+        'https://www-ebooks7-24-com.bdigital.sena.edu.co/stage.aspx?il=7120&pg=&ed=',
+    },
+    {
+      referencia:
+        'Carmona Rubio, G.,  y Díaz Corcobado, T.(2010). Electrónica aplicada. McGraw-Hill.',
+      link: 'https://www-ebooks7-24-com.bdigital.sena.edu.co/?il=4852',
+    },
+    {
+      referencia:
+        'Ruiz Vázquez, T.,  y Arbelaitz Gallego, O. (2004). Análisis Básico de circuitos eléctricos y electrónicos. Pearson Educación.',
+      link: 'https://www-ebooks7-24-com.bdigital.sena.edu.co/?il=4552',
+    },
+    {
+      referencia:
+        'Texas Instruments. (2015). Quad-Operational Amplifiers. [Hoja de datos] TI.',
+      link: 'https://www.ti.com/lit/ds/snosc16d/snosc16d.pdf',
     },
   ],
   creditos: [
@@ -131,13 +208,13 @@ export default {
       autores: [
         {
           nombre: 'Claudia Patricia Aristizabal',
-          cargo: 'Responsable del equipo',
+          cargo: 'Responsable del Equipo',
           centro: 'Dirección General',
         },
         {
-          nombre: 'Nombre completo',
-          cargo: 'Responsable de línea de producción',
-          centro: 'Nombre centro de formación',
+          nombre: 'Norma Constanza Morales Cruz',
+          cargo: 'Responsable de Línea de Producción',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
       ],
     },
@@ -145,9 +222,40 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Nombre centro de formación',
+          nombre: 'Ángela Rocío Sánchez Ruiz',
+          cargo: 'Experto Temático',
+          centro: 'Centro de Electricidad, Electrónica y Telecomunicaciones',
+        },
+        {
+          nombre: 'Miroslava González H.',
+          cargo: 'Diseñador y Evaluador Instruccional',
+          centro: 'Centro de Gestión Industrial',
+        },
+        {
+          nombre: 'Juan Gilberto Giraldo Cortés',
+          cargo: 'Diseñador Instruccional',
+          centro: '',
+        },
+        {
+          nombre: 'Álix Cecilia Chinchilla Rueda',
+          cargo: 'Evaluadora Instruccional',
+          centro: 'Centro de Gestión Industrial',
+        },
+        {
+          nombre: 'Viviana Esperanza Herrera Quiñones',
+          cargo: 'Asesora Metodológica',
+          centro: 'Centro de Comercio y Servicios',
+        },
+        {
+          nombre: 'Rafael Neftalí Lizcano',
+          cargo: 'Asesor Pedagógico',
+          centro:
+            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+        },
+        {
+          nombre: 'Darío González',
+          cargo: 'Revisión y Corrección de Estilo',
+          centro: 'Regional Tolima - Centro Agropecuario La Granja',
         },
       ],
     },
@@ -155,19 +263,29 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador web',
-          centro: 'Nombre centro de formación',
+          nombre: 'José Jaime Luis Tang',
+          cargo: 'Diseñador Web',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Francisco José Vásquez Suárez',
           cargo: 'Desarrollador Fullstack',
-          centro: 'Nombre centro de formación',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
         {
-          nombre: 'Nombre',
-          cargo: 'Animador y Producción audiovisual',
-          centro: 'Nombre centro de formación',
+          nombre: 'Gilberto Junior Rodríguez Rodríguez',
+          cargo: 'Storyboard e Ilustración',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
+        },
+        {
+          nombre: 'Nelson Iván Vera Briceño',
+          cargo: 'Animador y Producción Audiovisual',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
+        },
+        {
+          nombre: 'Sebastián Trujillo Afanador',
+          cargo: 'Actividad Didáctica',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
       ],
     },
@@ -175,14 +293,14 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Nombre validador',
-          cargo: 'Validación y vinculación en plataforma LMS',
-          centro: 'Nombre centro formación',
+          nombre: 'Javier Mauricio Oviedo',
+          cargo: 'Validación y Vinculación en Plataforma LMS',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
         {
-          nombre: 'Nombre validador',
-          cargo: 'Validación de contenidos accesibles',
-          centro: 'Nombre centro formación',
+          nombre: 'Gilberto Naranjo Farfán',
+          cargo: 'Validación de Contenidos Accesibles',
+          centro: 'Regional Tolima - Centro de Comercio y Servicios',
         },
       ],
     },
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -2,10 +2,26 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Órdenes de trabajo
+    .row.mt-4
+      .col-12.col-lg-8
+        p Las ordenes de trabajo representan el paso preliminar a la elaboración y ejecución de una tarea. Son documentos que contienen los lineamientos, especificaciones y orientaciones a tener en cuenta para la realización de una actividad. Cuando se habla de una orden de trabajo en procesos de diseño electrónico estas deben contener descritas las tareas y especificaciones del proceso a realizar.
+        .bloque-texto-a.color-primario-invertido.p-4.p-md-3.mt-4 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p.mb-0 Las ordenes de trabajo son muy variadas y depende del área en el que se utilice, contendrán actividades relacionadas. Existen ordenes de trabajo para diseño electrónico, ordenes de trabajo para realización de mantenimiento electrónico, ordenes de trabajo para prototipado mecánico ensamble electrónico desensamble electrónico entre otras.
+            .col-lg-4.mb-4.mb-lg-0
+              figure
+                img(src="@/assets/template/tema-3-1.svg", alt="Texto que describa la imagen").w-50.margin-0-auto
+    
+      .col-6.col-lg-4.offset-3.offset-lg-0
+        figure
+          img(src="@/assets/template/tema-3-2.png", alt="Texto que describa la imagen")
 
 </template>
 
@@ -26,4 +42,4 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass" scoped></style>
